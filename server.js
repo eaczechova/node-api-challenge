42989bbef83ab63f5d402fc2f241def9a6056906
Ewa Czech <ewa.czech@hotmail.com>
2020-05-16 18:15:43 +0200

    add script configuration

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send(`<h2>Hello from the app!</h2>`);
+});
+
+module.exports = server;
